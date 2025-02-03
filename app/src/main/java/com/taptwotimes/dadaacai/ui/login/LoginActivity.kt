a1dac285389a81e58bb84bd065167177f14c6b27
thiago.azevedo <thiago.azevedo@compasso.com.br>
2025-02-03 11:24:30 -0300

    create home layout

--- app/src/main/java/com/taptwotimes/dadaacai/ui/login/LoginActivity.kt
+++ app/src/main/java/com/taptwotimes/dadaacai/ui/login/LoginActivity.kt
@@ -1,13 +1,18 @@
 package com.taptwotimes.dadaacai.ui.login
 
+import android.credentials.CredentialOption
+import android.credentials.GetCredentialRequest
 import android.os.Bundle
 import androidx.activity.viewModels
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.taptwotimes.dadaacai.R
 import com.taptwotimes.dadaacai.databinding.ActivityLoginBinding
 import com.taptwotimes.dadaacai.ui.base.BaseActivity
 
-class LoginActivity: BaseActivity() {
-    private lateinit var binding: ActivityLoginBinding
 
+class LoginActivity: BaseActivity()  {
+    private lateinit var binding: ActivityLoginBinding
     private val viewModel:LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,17 @@ class LoginActivity: BaseActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        verifyButtonEnabled()
+        setButtonsClickListener()
     }
+
+    private fun setButtonsClickListener() {
+
+    }
+
+    private fun verifyButtonEnabled() {
+        TODO("Not yet implemented")
+    }
+
+
 }
