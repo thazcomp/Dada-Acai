5706a3425e64cf61819179a62cee05cce7c19613
thiago.azevedo <thiago.azevedo@compasso.com.br>
2025-02-03 15:22:40 -0300

    update home layout, add bottom navigation bar

--- app/src/main/java/com/taptwotimes/dadaacai/ui/home/HomeActivity.kt
+++ app/src/main/java/com/taptwotimes/dadaacai/ui/home/HomeActivity.kt
@@ -1,17 +1,35 @@
 package com.taptwotimes.dadaacai.ui.home
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.taptwotimes.dadaacai.R
 import com.taptwotimes.dadaacai.databinding.ActivityHomeBinding
 import com.taptwotimes.dadaacai.ui.base.BaseActivity
+import com.taptwotimes.dadaacai.ui.cart.CartFragment
 
 class HomeActivity: BaseActivity() {
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityHomeBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home_menu -> replaceFragment(HomeFragment())
+                //R.id.drawer_menu -> //Open Navigation Drawer
+                R.id.carrinho_menu -> replaceFragment(CartFragment())
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment).commit()
     }
 }
--- app/src/main/java/com/taptwotimes/dadaacai/ui/login/LoginActivity.kt
+++ app/src/main/java/com/taptwotimes/dadaacai/ui/login/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.taptwotimes.dadaacai.ui.login
 
+import android.content.Intent
 import android.credentials.CredentialOption
 import android.credentials.GetCredentialRequest
 import android.os.Bundle
@@ -9,6 +10,7 @@ import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.taptwotimes.dadaacai.R
 import com.taptwotimes.dadaacai.databinding.ActivityLoginBinding
 import com.taptwotimes.dadaacai.ui.base.BaseActivity
+import com.taptwotimes.dadaacai.ui.home.HomeActivity
 
 
 class LoginActivity: BaseActivity()  {
@@ -21,12 +23,14 @@ class LoginActivity: BaseActivity()  {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        verifyButtonEnabled()
         setButtonsClickListener()
     }
 
     private fun setButtonsClickListener() {
-
+        binding.btEntrar.setOnClickListener {
+            val intent =  Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun verifyButtonEnabled() {
