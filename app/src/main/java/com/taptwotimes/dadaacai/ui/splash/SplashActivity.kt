4b28778de9b1930034ee95cbbd5dded90aa434d9
thiago.azevedo <thiago.azevedo@compasso.com.br>
2025-03-12 10:48:42 -0300

    request permissions

--- app/src/main/java/com/taptwotimes/dadaacai/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/taptwotimes/dadaacai/ui/splash/SplashActivity.kt
@@ -1,11 +1,15 @@
 package com.taptwotimes.dadaacai.ui.splash
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.drawable.AnimationDrawable
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.taptwotimes.dadaacai.R
 import com.taptwotimes.dadaacai.databinding.ActivitySplashBinding
 import com.taptwotimes.dadaacai.ui.base.BaseActivity
@@ -16,6 +20,9 @@ class SplashActivity: BaseActivity(){
 
     private lateinit var binding:ActivitySplashBinding
 
+    private val STORAGE_PERMISSION_CODE = 101
+    private val LOCATION_PERMISSION_CODE = 102
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,13 +32,52 @@ class SplashActivity: BaseActivity(){
         playSound(R.raw.som_double_click, 850)
         playSound(R.raw.som_magic_shine, 2400)
 
-        val animation:AnimationDrawable = binding.ivSplash.background as AnimationDrawable
-        animation.start().also {
-            goToMainActivity()
-        }
-
+        requestStoragePermission()
     }
 
+    private fun requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+        }else{
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSION_CODE)
+        }else{
+            val animation:AnimationDrawable = binding.ivSplash.background as AnimationDrawable
+            animation.start().also {
+                goToMainActivity()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            STORAGE_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    requestLocationPermission()
+                } else {
+                    requestStoragePermission()
+                }
+            }
+            LOCATION_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    val animation:AnimationDrawable = binding.ivSplash.background as AnimationDrawable
+                    animation.start().also {
+                        goToMainActivity()
+                    }
+                }else{
+                    requestLocationPermission()
+                }
+            }
+        }
+    }
 
 
     private fun goToMainActivity() {
