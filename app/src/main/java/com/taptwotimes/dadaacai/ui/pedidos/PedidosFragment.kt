b393807d4892f2911dd9dacb617cfdbd9d14191d
thiago.azevedo <thiago.azevedo@compasso.com.br>
2025-03-15 15:26:08 -0300

    create layout pedidos

---
+++ app/src/main/java/com/taptwotimes/dadaacai/ui/pedidos/PedidosFragment.kt
@@ -0,0 +1,9 @@
+package com.taptwotimes.dadaacai.ui.pedidos
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PedidosFragment:Fragment() {
+
+}
